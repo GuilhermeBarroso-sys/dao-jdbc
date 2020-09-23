ce1de46a7570183a0aa4fac9eaa69e6f224324ec
GolfinhoNinja <golfinhoninja@hotmail.com>
2020-09-23 18:24:11 -0300

    Create Seller Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Gui", "gui@gmail.com", new Date(), 3000.00, obj);
+		System.out.println("Seller: " + seller);
 	}
 }
