9553925d7f3321995de785ebde1b59e99962c566
GolfinhoNinja <golfinhoninja@hotmail.com>
2020-09-24 14:13:36 -0300

    SellerDaoJDBC, DaoFactory and some erros fixed.

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,14 +2,16 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		
 		Seller seller = new Seller(21, "Gui", "gui@gmail.com", new Date(), 3000.00, obj);
 		System.out.println("Seller: " + seller);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 }
